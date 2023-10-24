518d03aecbe98dc56494c2964270339462384c6a
Dario Breitenstein <dario@imakethings.ch>
2023-10-24 13:37:27 +0200

    fix beacon rendering

---
+++ lib/providers/model/beacon_provider.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+
+import 'package:enter_cms_flutter/models/beacon.dart';
+import 'package:enter_cms_flutter/providers/services/cms_api_provider.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'beacon_provider.g.dart';
+
+@Riverpod(
+  dependencies: [
+    cmsApi,
+  ],
+)
+class Beacon extends _$Beacon {
+  Future<MBeacon> _fetchBeacon(int id) async {
+    final api = ref.watch(cmsApiProvider);
+    return await api.getBeacon(id);
+  }
+
+  @override
+  FutureOr<MBeacon> build(int id) async {
+    return _fetchBeacon(id);
+  }
+}
